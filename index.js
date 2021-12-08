4f5fc3406c091f40abc442a4d783345699d88158
Sarah-Joy Roxborough <sarahjroxb@gmail.com>
2021-12-07 20:16:12 -0500

    Included assignment instructions in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// YOUR CODE HERE
+// Create a class named car
+
+
+// Use class to create objects that have 3 properties
+
+
+// Include 'honk' and 'beep' methods
+
+
+// Create a variable named mySweetRide and assign it to a car created with your class using the following arguments:
+// Make: 'Pontiac'
+// Model: 'Fiero'
+// Make: 1988
+
+
+// Call mySweetRide's 'honk' method once
+
+
+// Call mySweetRide's performMaintenance method once 
